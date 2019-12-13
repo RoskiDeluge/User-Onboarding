f687fc147538b7a44a6a4b21a33331c67b1820db
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-12-12 17:35:19 -1000

    Finished step 2 - basic field validation

--- userly/src/components/Form.js
+++ userly/src/components/Form.js
@@ -5,8 +5,11 @@ import axios from "axios";
 
 const UserForm = ({ values, errors, touched, status }) => {
 
+  const [users, setUsers] = useState([]);
 
-
+  useEffect(() => {
+    status && setUsers(users => [...users, status]);
+  }, [status]);
 
   return (
     <div>
@@ -20,6 +23,9 @@ const UserForm = ({ values, errors, touched, status }) => {
             name="name"
             placeholder="Enter your name"
           />
+          {touched.name && errors.name && (
+            <p>{errors.name}</p>
+          )}
         </label>
         <label htmlFor="email">
           Email 
@@ -29,6 +35,9 @@ const UserForm = ({ values, errors, touched, status }) => {
             name="email"
             placeholder="Enter your email"
           />
+          {touched.email && errors.email && (
+            <p>{errors.email}</p>
+          )}
         </label>
         <label>
           Password 
@@ -38,6 +47,9 @@ const UserForm = ({ values, errors, touched, status }) => {
             name="password"
             placeholder="Enter your password"
           />
+          {touched.password && errors.password && (
+            <p>{errors.password}</p>
+          )}
         </label>
         <label>
           Terms of Service
@@ -54,5 +66,40 @@ const UserForm = ({ values, errors, touched, status }) => {
 
 };
 
-export default UserForm
+const FormikUserForm = withFormik({
+  // props from <AnimalForm /> in app are in props param
+    mapPropsToValues(props) {
+      // set initial state of form to value from parent component OR the initial value (after || )
+      return {
+        name: props.name || "",
+        email: props.size || "",
+        password: props.diet || "",
+        tos: props.tos || false
+      };
+    },
+
+    validationSchema: Yup.object().shape({
+      name: Yup.string().required("Name is required"),
+      // passing a string in required makes a custom inline error msg
+      email: Yup.string().required("Email is required"),
+      password: Yup.string().required("Password is required")
+    }),
+
+    handleSubmit(values, { setStatus, resetForm }) {
+      console.log("submitting", values);
+      axios
+        .post("https://reqres.in/api/users/", values)
+        .then(res => {
+          console.log("success", res);
+          // sends a status update through props in AnimalForm with value as res.data content
+          setStatus(res.data);
+  
+          //clears form inputs, from FormikBag
+          resetForm();
+        })
+        .catch(err => console.log(err.response));
+    }
+  })(UserForm);
+
+export default FormikUserForm
 
